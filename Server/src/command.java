685752d4b50001a4d28ffb4affa6c51fa9968d69
FatimaMel <fatimamellata@yahoo.ca>
2023-01-19 21:09:25 -0500

    enum fonctionnality

--- Server/src/command.java
+++ Server/src/command.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class command {
+	public void createDirectory(String nameDirectory) throws IOException{
+		String dirName = nameDirectory;
+		
+		Path path = Paths.get(dirName);
+		
+		if (Files.notExists(path)){
+			Files.createDirectory(path);
+		}
+	}
 
+	enum Fonctionnality{
+		cd, 
+		ls,
+		mkdir,
+		upload,
+		download,
+		exit
+	}
+	
 }
